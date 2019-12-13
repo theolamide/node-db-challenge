91470dcc12283af8142c977837967c94130e3342
theolamide <ajadidan@gmail.com>
2019-12-13 18:30:11 -0500

    All helper functions added. New Task add not working yet though

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,1 +1,80 @@
 const db = require('../data/db-config');
+
+module.exports ={
+    find,   //Project-level
+    add,    //Project-level
+    findById,   //Project-level
+    findResources,
+    addResource,
+    findResourceById,
+    findTasksByProjectId,
+    addTask
+}
+
+function find(){
+    return db('projects')
+}
+
+function findById(id){
+    return db('projects')
+        .where({id})
+        .first()
+}
+
+function add(project){
+    return db('projects')
+        .insert(project,'id')
+        .then(ids => {
+            const [id] = ids;
+            return findById(id)
+        })
+}
+
+function findResources() {
+    return db("resources");
+    }
+
+function findResourceById(id) {
+    return db("resources")
+        .where({ id })
+        .first();
+    }
+
+function addResource(resource) {    
+    return db("resources")
+        .insert(resource, 'id')
+        .then(ids => {
+            const [id] = ids;
+        return findResourceById(id);
+        });
+    }
+
+function findTasksByProjectId(projectId) {
+    // select p.projectName
+    // , p.projectDescription
+    // , * from task 
+    // join  projects as p
+    // on p.id = task.project_id;
+
+    // let TaskStatus = db('task.status')
+
+    // if (TaskStatus.toString === '0'){
+    //     TaskStatus = 'Not Completed'
+    //     } else {TaskStatus = 'Completed'}
+    
+    return db('task')
+        .select('p.projectName','p.projectDescription','task.id as taskId','task.taskDescription','task.notes','task.status')
+        .join('projects as p','p.id', 'task.project_id')
+        .where('project_id', projectId)
+}
+
+function addTask(task){
+    return db('task')
+        .insert(task,'id')
+        // .then(ids => {
+        //     const [id] = ids;
+        //     return findById(id)
+        // })
+}
+
+
--- projects/project-router.js
+++ projects/project-router.js
@@ -4,7 +4,102 @@ const Projects = require('./project-model.js');
 
 const router = express.Router();
 
+router.get('/', (req,res) => {
+    Projects.find()
+    .then(projects =>{
+        res.json(projects);
+    })
+    .catch(err => {
+        res.status(500)
+        .json({message: 'Failed to load project list'})
+    })
+})
 
+router.post('/', (req,res) => {
+    const projectData = req.body;
+
+    Projects.add(projectData)
+        .then(project =>{
+            res.status(201)
+            .json(project);
+        })
+        .catch (err => {
+            res.status(500)
+            .json({ message: 'Failed to create new project' });
+        });
+})
+
+router.get("/resources", (req, res) => {
+    Projects.findResources()
+        .then(resources => {
+            res.status(200)
+            .json(resources);
+        })
+        .catch(err => {
+            res.status(500)
+            .json({ errorMessage: "Failed to get resources" });
+        });
+});
+
+router.post("/:id/resources", (req, res) => {
+    const { id } = req.params;
+    const newResource = req.body;
+
+    if (!newResource.resourceName) {
+        res.status(400)
+        .json({errorMessage: "Please provide a name for the resource."
+        });
+    } else {
+        Projects.addResource(newResource, id)
+        .then(resource => {
+            res.status(201)
+            .json(resource);
+        })
+        .catch(err => {
+            res.status(500)
+            .json({errorMessage: "Failed to add resource"
+        });
+        });
+    }
+});
+
+router.get('/:id/tasks', (req,res) => {
+    const {id} = req.params
+
+    Projects.findTasksByProjectId(id)
+    .then(tasks => {
+        if (tasks) {
+            res.json(tasks)
+        } else {
+            res.status(404)
+            .json({message: 'Could not find tasks for given project id'})
+        }
+    })
+    .catch(err => {
+        res.status(500)
+        .json({ message: 'Failed to get tasks' });
+    });
+})
+
+router.post('/:id/tasks',(req,res) => {
+    const {id} = req.params;
+    const newTask = req.body;
+
+    if(!newTask.taskDescription){
+        res.status(400)
+        .json({message: 'The task description cannot be left null. Please provide some direction.'})
+    } else {
+        Projects.addTask(newTask)
+        .then(task => {
+            res.status(201)
+            .json(task);
+        })
+        .catch(error => {
+            res.status(500)
+            .json({errorMessage: 'Failed to add tasks'})
+        })
+    }
+})
 
 
 
